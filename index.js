adbac4527759d7686b3394fbdf9297ded2755a60
Fabrizio Lallo <fabrizio.lallo@konicaminolta.it>
2022-02-24 19:24:40 +0100

    refactor: renamed some variables

--- index.js
+++ index.js
@@ -4,30 +4,29 @@ import emoji from 'node-emoji';
 class EmojiMoonStream extends Readable {
   constructor (options) {
     super(options);
-    this.moon = [];
+    this.moonPhases = [];
     this.index = 0;
-    this.moon.push(emoji.get(':full_moon:'));
-    this.moon.push(emoji.get(':waning_gibbous_moon:'));
-    this.moon.push(emoji.get(':last_quarter_moon:'));
-    this.moon.push(emoji.get(':waning_crescent_moon:'));
-    this.moon.push(emoji.get(':new_moon:'));
-    this.moon.push(emoji.get(':waxing_crescent_moon:'));
-    this.moon.push(emoji.get(':first_quarter_moon:'));
-    this.moon.push(emoji.get(':moon:'));
+    this.moonPhases.push(emoji.get(':full_moon:'));
+    this.moonPhases.push(emoji.get(':waning_gibbous_moon:'));
+    this.moonPhases.push(emoji.get(':last_quarter_moon:'));
+    this.moonPhases.push(emoji.get(':waning_crescent_moon:'));
+    this.moonPhases.push(emoji.get(':new_moon:'));
+    this.moonPhases.push(emoji.get(':waxing_crescent_moon:'));
+    this.moonPhases.push(emoji.get(':first_quarter_moon:'));
+    this.moonPhases.push(emoji.get(':moon:'));
   }
 
   _read () {
-    this.push(this.moon[this.index++]);
+    this.push(this.moonPhases[this.index++]);
     if (this.index === 8) {
       this.index = 0;
     }
   }
 }
 const server = http.createServer((req, res) => {
-  const moonStream = new EmojiMoonStream();
+  const moonPhasesStream = new EmojiMoonStream();
 
-  pipeline(moonStream, res, (error) => console.log(error));
+  pipeline(moonPhasesStream, res, (error) => console.log(error));
 });
 
-server.listen(3000)
-;
+server.listen(3000);
